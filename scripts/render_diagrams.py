b749fb5cd4e404f3783ea45104d2326edcdbaf78
Alexandre Couedelo <nokwebspace@gmail.com>
2023-07-26 18:03:11 +0100

    feat: generate images (#1)
    * feat: generate excalidraw
    
    * fix: github action
    
    * feat: add generate step to workflow
    
    * feat: allow script to run on pr
    
    * fix: kroki path
    
    * fix: kroki path
    
    * fix: invert kroki and checkout step
    
    * fix: use folder in puml folder
    
    * feat: hugo init
    
    * feat: add theme liva
    
    * wip: try to make the theme work
    
    * fix: got the theme working
    
    * feat: generate image with local kroki server
    
    * fix: kroki.yml was ignored
    
    * feat: generate image with python script instead
    
    * feat: wait for server to be fully started
    
    * fix: make sure the gallery folder exists

---
+++ scripts/render_diagrams.py
@@ -0,0 +1,44 @@
+import os            
+import subprocess
+import shutil
+import yaml
+
+gallery_path="website/static/images/gallery"
+gallery_data_path="website/data/gallery.yml"
+
+
+gallery_config = {
+    "enable": True,
+    "gallery": []
+}
+
+folders = ["excalidraw", "plantuml"]
+
+# traverse root directory, and list directories as dirs and files as files
+for folder in folders:
+    for root, dirs, files in os.walk(folder):
+        path = root.split(os.sep)
+        print((len(path) - 1) * '---', os.path.basename(root))
+        for file in files:
+            filePath = os.path.splitext(file)
+            input = os.path.join(root, file)
+            output = os.path.join(root, filePath[0]) + ".svg"
+            output_dest = os.path.join(gallery_path, filePath[0]) + ".svg"
+            if filePath[1] in [".svg", ".png"]:
+                continue
+            print(len(path) * '---', filePath)
+            subprocess.run(
+                f"./kroki convert {input} --type {folder}",
+                shell=True, check=True
+            )
+
+            shutil.copyfile(output, output_dest)
+
+            gallery_config["gallery"].append({
+                "image": "images/gallery/" + filePath[0] + ".svg"
+            })
+
+            with open(gallery_data_path, 'w') as file:
+                documents = yaml.dump(gallery_config, file)
+
+ 
