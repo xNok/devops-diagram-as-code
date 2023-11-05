2dc2626d87ab8888bea29858082c5dfe966fad8a
Alexandre Couedelo <a.couedelo@efg.gg>
2023-11-05 20:55:35 +0000

    chore: migrate cms to tinacms

---
+++ website/tina/config.ts
@@ -0,0 +1,157 @@
+import { defineConfig } from "tinacms";
+import { portfolioFields } from "./templates";
+import { postFields } from "./templates";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: null, // Get this from tina.io
+  token: null, // Get this from tina.io
+  client: { skip: true },
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        format: "md",
+        label: "Post",
+        name: "post",
+        path: "exampleSite/content/blog",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...postFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Portfolio",
+        name: "portfolio",
+        path: "exampleSite/content/portfolio",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          allowedActions: {
+            create: false,
+          },
+        },
+      },
+      {
+        format: "md",
+        label: "About",
+        name: "about",
+        path: "exampleSite/content/about",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          allowedActions: {
+            create: false,
+          },
+        },
+      },
+      {
+        format: "yml",
+        label: "Data",
+        name: "data",
+        path: "exampleSite/data",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        format: "md",
+        label: "Contact",
+        name: "contact",
+        path: "exampleSite/content/contact",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          allowedActions: {
+            create: false,
+          },
+        },
+      },
+      {
+        format: "toml",
+        label: "Configuration",
+        name: "configuration",
+        path: "exampleSite",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
